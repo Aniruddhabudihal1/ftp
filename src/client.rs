09e92590cdefc5b94cf7093242830c6508ac7a6b
Aniruddha Budihal <aniruddhabudihal@gmail.com>
2026-03-27 11:49:44 +0530

    basic stable functioning version done. Need to refine more and filter out bugs

--- src/client.rs
+++ src/client.rs
@@ -2,35 +2,24 @@
 
 use core::panic;
 use std::{
-    io::{BufRead, BufReader, Read, Write},
+    fs::File,
+    io::{self, BufRead, BufReader, Write},
     net::TcpStream,
 };
 
 pub fn client_call() {
-    let msg = String::from("hello\n");
-
-    let mut con = TcpStream::connect("127.0.0.0:8080").unwrap();
-
-    //let mut con_read = TcpListener::bind("127.0.0.0:8080").unwrap();
-    //let tmp = con_read.accept().unwrap();
-    //let mut stream = tmp.0;
+    let conn = TcpStream::connect("127.0.0.0:8080").unwrap();
+    let mut buf_reader = BufReader::new(&conn);
 
     // Sends SYN message to client
-    let ret = con.write(msg.as_bytes());
-
-    match ret {
-        Ok(_) => println!("the write worked"),
-        Err(er) => panic!(
-            "the write did not work, and returned the following error : {}",
-            er
-        ),
-    }
-
-    let mut server_output_buffer = String::new();
+    let con_ref = &mut conn.try_clone().unwrap();
+    write_to_server(con_ref, String::from("hello\n"));
 
     // reading ACK message from the server
-    let mut reader = BufReader::new(con);
-    let ret = reader.read_line(&mut server_output_buffer);
+    //let ret = conn.read_exact(server_output_buffer.as_bytes());
+    let server_output_buffer = read_from_server(&mut buf_reader);
+
+    /*
     match ret {
         Ok(_) => println!("the read_line worked"),
         Err(er) => panic!(
@@ -38,25 +27,129 @@ pub fn client_call() {
             er
         ),
     };
-
-    println!("{}", server_output_buffer);
+    */
+    println!("test");
+    println!("servers output : {}", server_output_buffer);
 
     if server_output_buffer == "hello client" {
-        println!("Connection with the server confirmed");
+        println!("Connection with the server confirmed with the ACK message");
     } else {
         panic!("connection with the server failed");
     }
 
+    // reading file name from user input and then sending it to the server
+    let mut file_name = String::new();
+    println!("Enter file to be searched : ");
+    io::stdin()
+        .read_line(&mut file_name)
+        .expect("was not able to read file name for some reason");
+
+    write_to_server(con_ref, file_name.clone());
     /*
-        let _ = con.read_to_string(&mut server_output_buffer);
-        println!(
-            "probably this will just be empty : {}",
-            server_output_buffer
-        );
-        if server_output_buffer == "hello client" {
-            println!("Connection with the server confirmed");
-        } else {
-            panic!("connection with the server failed");
-        }
+        let ret = writer.write(file_name.as_bytes());
+
+        match ret {
+            Ok(_) => println!("successfully sent the file name to the server"),
+            Err(er) => panic!(
+                "The following error ocurred while sending file name to the string : {}",
+                er
+            ),
+        };
     */
+
+    let server_resp_for_file_sent = read_from_server(&mut buf_reader);
+    if server_resp_for_file_sent == "true\n" {
+        println!("the file exists and proceeding to ask the server to send the file");
+    } else {
+        panic!("the requested file does not exist");
+    }
+
+    /*
+    let mut initial_file_response = String::new();
+    let ret = con_ref.read(&mut initial_file_response.as_bytes());
+    match ret {
+        Ok(_) => {
+            println!("successfully recieved a response from the server");
+            if initial_file_response == "true\n" {
+                println!("the file exists and proceeding to ask the server to send the file");
+            } else {
+                panic!(
+                    "the file does not exist, later going to implement something where it goes back to asking the client to enter another file name"
+                );
+                //TODO
+            }
+        }
+        Err(er) => panic!(
+            "The following error ocurred while recieving the file status from the server : {}",
+            er
+        ),
+    };
+    */
+
+    /*
+        let cont = String::from("continue\n");
+        let ret = writer.write(cont.as_bytes());
+        match ret {
+            Ok(_) => println!("successfully sent the file confirmation to the server"),
+            Err(er) => panic!(
+                "The following error ocurred while sending the file confirmation to the string : {}",
+                er
+            ),
+        };
+    */
+
+    write_to_server(con_ref, String::from("continue\n"));
+
+    let file_content = read_from_server(&mut buf_reader);
+
+    /*
+    let ret = con_ref.read_exact(&mut file_content.as_bytes());
+    match ret {
+        Ok(bytes_recieved) => println!("successfully recieved content from the server",),
+        Err(er) => panic!(
+            "something went wrong while trying to read from the server\nthe following is the errror recieved : {}",
+            er
+        ),
+    };
+    */
+
+    let ret = File::create_new(file_name);
+    match ret {
+        Ok(mut f) => {
+            let _ = f.write_all(file_content.as_bytes());
+            println!("successfully created the file !! ");
+        }
+        Err(er) => panic!(
+            "check if the file already exists in the current directory, got error : {}",
+            er
+        ),
+    };
+}
+
+fn write_to_server(conn: &mut TcpStream, msg: String) {
+    let ret = conn.write_all(msg.as_bytes());
+    match ret {
+        Ok(_) => {
+            println!("Write to the server commited successfully, where bytes were transferred")
+        }
+        Err(er) => println!(
+            "Something went wrong while sending a message to the server\nerror : {}",
+            er
+        ),
+    };
+}
+
+fn read_from_server(conn: &mut BufReader<&TcpStream>) -> String {
+    // TODO
+    let first_ret = conn.lines().next();
+    match first_ret {
+        Some(ret) => match ret {
+            Ok(final_string) => final_string,
+            Err(er) => panic!(
+                "something went wrong while reading from the server\nerror returned : {}",
+                er
+            ),
+        },
+        None => String::new(),
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -2,23 +2,24 @@
 
 use core::panic;
 use std::{
-    io::{BufRead, BufReader, Write},
-    net::TcpListener,
+    fs::File,
+    io::{BufRead, BufReader, Read, Write},
+    net::{TcpListener, TcpStream},
+    path::{self, PathBuf},
+    str::FromStr,
 };
 
 pub fn server_implementation() {
     let conn = TcpListener::bind("127.0.0.0:8080").unwrap();
-    let listen = conn.accept().unwrap();
-    let mut stream = listen.0;
+    let mut stream = conn.accept().unwrap().0;
 
-    let mut reader = BufReader::new(&stream);
-
-    //let mut con_stream = TcpStream::connect("127.0.0.0:8080").unwrap();
+    let con_ref = &mut stream.try_clone().unwrap();
+    let mut reader = BufReader::new(con_ref);
+    //let mut writer = BufWriter::new(&stream);
 
     // recieving the SYN message from the client
     let mut buf = String::new();
 
-    // this line works only when I control+c on the client
     let ret = reader.read_line(&mut buf);
     match ret {
         Ok(_) => println!("The read_line worked"),
@@ -30,18 +31,106 @@ pub fn server_implementation() {
 
     println!("{}", buf);
     // responding with the ACK message to client
-    if buf == "hello\n".to_string() {
-        let _ = stream.write_all(String::from("hello client").as_bytes());
+    if buf == "hello\n" {
+        //let _ = writer.write_all(String::from("hello client").as_bytes());
+        write_to_client(&mut stream, String::from("hello client\n"));
     } else {
         panic!("failed to recieve an appropriate SYN message from the client");
     }
 
+    // The client has sent what file to be transferred, and then based on availablity the file will
+    // be transferred
+    //let file_name = read_from_client(reader);
+    let mut file_name = String::new();
+    let ret = reader.read_line(&mut file_name);
+    println!("File requested by the client is : {}", file_name);
+    match ret {
+        Ok(_) => println!("successfully read the clients desired file name"),
+        Err(er) => panic!(
+            "was not able to read the file name sent from the client and gave the following errror : {}",
+            er
+        ),
+    };
+
+    let file_path = path::PathBuf::from_str(&file_name).unwrap();
+    let test = path::Path::is_file(&file_path.canonicalize().unwrap());
+    println!("file exists status : {}", test);
+
+    let ret = File::open(&file_name);
+    match ret {
+        Ok(_tmp) => {
+            println!("The file exists on the server, and confirmation being sent to the client");
+            let status = String::from("true\n");
+            write_to_client(&mut stream, status);
+            //let _ = writer.write_all(status.as_bytes());
+        }
+        Err(_) => {
+            println!("The requested file does not exist on the server :( ");
+            let status = String::from("false\n");
+            write_to_client(&mut stream, status);
+            //let _ = writer.write_all(status.as_bytes());
+        }
+    };
+
+    let mut conf = String::new();
+    let ret = reader.read_line(&mut conf);
+    match ret {
+        Ok(_) => println!("successfully read the clients confirmation for sending the file"),
+        Err(er) => panic!(
+            "was not able to read the file confirmation sent from the client and gave the following errror : {}",
+            er
+        ),
+    };
+
+    let mut file_content = String::new();
+    let smt = File::open(&file_name);
+    let mut f = match smt {
+        Ok(fo) => {
+            println!("was able to open the file");
+            fo
+        }
+        Err(er) => panic!(
+            "file path passed in is : {}\nfile probably does not exist : {}",
+            file_name, er
+        ),
+    };
+    let ret = f.read_to_string(&mut file_content);
+    match ret {
+        Ok(_) => {
+            println!("successfully read the files contents and is ready to be sent to the client")
+        }
+        Err(er) => panic!(
+            "Something went wrong while reading from the file, it gave the following errror : {}",
+            er
+        ),
+    };
+
+    write_to_client(&mut stream, file_content);
+
     /*
-    *
-    let writing_con = TcpStream::connect("127.0.0.0:8080");
-    match writing_con {
-        Ok(peace) => println!("will be able to write to {:?}", peace),
-        Err(er) => println!("Cannot write :( {}", er),
-    }
+    let ret = writer.write(file_content.as_bytes());
+    match ret {
+        Ok(bytes_sent) => println!(
+            "File content transfer complete, with {} bytes transmitted",
+            bytes_sent
+        ),
+        Err(er) => panic!(
+            "somethign went wrong while sending content to the client\nit gave the following error : {}",
+            er
+        ),
+    };
     */
+}
+
+fn write_to_client(conn: &mut TcpStream, msg: String) {
+    let ret = conn.write_all(msg.as_bytes());
+    match ret {
+        Ok(_) => {
+            println!("Write to the server commited successfully, where bytes were transferred")
+        }
+        Err(er) => println!(
+            "Something went wrong while sending a message to the server\nerror : {}",
+            er
+        ),
+    };
 }
