a9930137d8a75d77070216cb8f7e2d93d796b6c0
Aniruddha Budihal <aniruddhabudihal@gmail.com>
2026-03-30 18:38:14 +0530

    revisions to main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use crate::{client::client_call, server::server_implementation};
-use std::env;
+use std::io;
 
 mod client;
 mod server;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    match args.get(2) {
-        Some(_) => client_call(),
-        _ => server_implementation(),
+    println!("what do you want to run ?\nclient or server");
+    let mut state = String::new();
+    io::stdin().read_line(&mut state).unwrap();
+    if state.to_lowercase().trim() == "server" {
+        server_implementation();
+    } else {
+        client_call();
     }
 }
